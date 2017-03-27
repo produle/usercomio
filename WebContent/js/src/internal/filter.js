1d428350409d006f940355e09d8793c73d19c2bd
vgnanasabapathy <vgnanasabapathy@produle.com>
2017-03-27 10:58:51 +0530

    UC-39 Listing Page Filter UI Changes

--- WebContent/js/src/internal/filter.js
+++ WebContent/js/src/internal/filter.js
@@ -162,6 +162,13 @@ function UC_FilterController()
         $("#ucFilterName").val(filterObj.name);
 
         $("#ucEditFilterModal").modal();
+        $('.rule-container .rule-actions .btn-danger').text('');
+		$('.rule-container .rule-actions .btn-danger').addClass('ucListingFilterDeleteIcon');
+		
+        $('#ucFilterQueryBuilderUI').on('afterAddRule.queryBuilder', function(e, rule, error, value) {
+        	$('.rule-container .rule-actions .btn-danger').text('');
+        	$('.rule-container .rule-actions .btn-danger').addClass('ucListingFilterDeleteIcon');
+        });
     };
 
     /*
@@ -194,6 +201,15 @@ function UC_FilterController()
         $("#ucFilterName").val("");
 
         $("#ucEditFilterModal").modal();
+        
+        $('.rule-container .rule-actions .btn-danger').text('');
+		$('.rule-container .rule-actions .btn-danger').addClass('ucListingFilterDeleteIcon');
+		
+        $('#ucFilterQueryBuilderUI').on('afterAddRule.queryBuilder', function(e, rule, error, value) {
+        	$('.rule-container .rule-actions .btn-danger').text('');
+        	$('.rule-container .rule-actions .btn-danger').addClass('ucListingFilterDeleteIcon');
+        });
+        
     };
 
     /*
