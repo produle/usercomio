a66160ce51db0315ad081612dea45be87773724a
KavinKumar C <ckavin@produle04s-iMac.local>
2017-06-16 17:27:51 +0530

    Testing dynamic branch build

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
+Branch="$(git rev-parse --abbrev-ref HEAD)"
 setup_git() {
   git config --global user.email "ckavin@produle.com"
   git config --global user.name "ckavinkumar"
 }
 
-commit_website_files() {
-  Branch="$(git rev-parse --abbrev-ref HEAD)"
+commit_website_files() { 
   git checkout $Branch
   git add WebContent/dist/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
@@ -12,7 +12,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN2}@github.com/ckavinkumar/usercomio.git > /dev/null 2>&1 
-  git push origin-pages master  
+  git push origin-pages $Branch  
 }
 
 setup_git
