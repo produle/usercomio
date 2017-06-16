5f303a0c57be4b9a9eb5bd065dde40e0e55fe5c1
KavinKumar C <ckavin@produle04s-iMac.local>
2017-06-16 16:24:10 +0530

    Testing dynamic branch

--- push.sh
+++ push.sh
@@ -1,16 +1,17 @@
 setup_git() {
-  git config --global user.email "pbaskaran@produle.com"
-  git config --global user.name "pbaskaran"
+  git config --global user.email "ckavin@produle.com"
+  git config --global user.name "ckavinkumar"
 }
 
 commit_website_files() {
-  git checkout master 
+  Branch="$(git rev-parse --abbrev-ref HEAD)"
+  git checkout $Branch
   git add WebContent/dist/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/produle/usercomio.git > /dev/null 2>&1 
+  git remote add origin-pages https://${GH_TOKEN2}@github.com/ckavinkumar/usercomio.git > /dev/null 2>&1 
   git push origin-pages master  
 }
 
