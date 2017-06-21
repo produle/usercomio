6e72c1c5720c7196e2fe5b053f2381a511dd7922
ckavin <ckavin@192.168.1.3>
2017-06-21 18:39:01 +0530

    UC-73  - TRAVIS_Branch check

--- push.sh
+++ push.sh
@@ -14,8 +14,9 @@ commit_website_files() {
 
 upload_files() {
   Branch="$(git rev-parse --abbrev-ref HEAD)"
-  echo "current Branch=" $Branch
-  echo "TRAVIS_BRANCH=" $TRAVIS_BRANCH
+  echo "current Branch=" $Branch 
+  TRAVIS_Branch="$(TRAVIS_BRANCH)"
+  echo "TRAVIS_Branch=" $TRAVIS_Branch
   git remote add origin-pages https://${GH_TOKEN}@github.com/ckavinkumar/usercomio.git > /dev/null 2>&1 
   git push origin-pages $Branch  
 }
