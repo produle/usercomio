e1ec18a7fd6e05048453f41fd62b9ef07ab72a58
ckavin <ckavin@192.168.1.7>
2017-07-05 10:55:01 +0530

    UC-73 Make Travis CI update branch dynamic

--- push.sh
+++ push.sh
@@ -1,19 +1,18 @@
  
 setup_git() {
-  git config --global user.email "ckavin@produle.com"
-  git config --global user.name "ckavinkumar"
+  git config --global user.email "pbaskaran@produle.com"
+  git config --global user.name "pbaskaran"
 }
 
 commit_website_files() { 
-  git fetch -p origin
-  Branch="$(git rev-parse --abbrev-ref HEAD)"
+  git fetch -p origin 
   git checkout $TRAVIS_BRANCH
   git add WebContent/dist/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() { 
-  git remote add origin-pages https://${GH_TOKEN}@github.com/ckavinkumar/usercomio.git > /dev/null 2>&1 
+  git remote add origin-pages https://${GH_TOKEN}@github.com/produle/usercomio.git > /dev/null 2>&1 
   git push origin-pages $TRAVIS_BRANCH
 }
 
